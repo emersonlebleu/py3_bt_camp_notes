58ffbf89b4cc9d609ba69cc13fb32362324a59bd
Emerson Lebleu <emerson.lebleu@gmail.com>
2020-12-10 07:06:47 -0700

    Adding modules notes, not complete taking about color printing to screen

---
+++ modules.py
@@ -0,0 +1,21 @@
+from colorama import init
+from termcolor import colored
+#----Notes on Modules----
+
+#writing code in one file and then using that within another file
+#modules help to keep our python code small
+#module is just another python file
+
+#custom, builtin, external modules 
+from random import choice, randint
+print(choice(["pink", "purple", "blue", "yellow"]))
+
+#----Custom Modules----
+#same syntax from built in
+#if it isnt in the right directory you need to reference the path to the file
+
+#calling init because this strips the ANSI escape sequences and replaces with wind32 calls
+init()
+
+print(colored("My Dog is Rad!", "white", "on_blue"))
+
