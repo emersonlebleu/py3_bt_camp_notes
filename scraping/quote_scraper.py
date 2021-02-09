ddfadfd08ef487cef8081ad015e985f7a0910f2a
Emerson Lebleu <emerson.lebleu@gmail.com>
2021-02-09 06:52:31 -0700

    finishing up the quote scraping

--- scraping/quote_scraper.py
+++ scraping/quote_scraper.py
@@ -1,27 +1,69 @@
-#-----Instuctions-----#
-# Create a file called `scraping_project.py` which, when run, grabs data on every quote from 
-# the website http://quotes.toscrape.com
-# You can use `bs4` and `requests` to get the data. 
+import requests #module used to pull html from a website
+from bs4 import BeautifulSoup #allows us to work with html in python
+from time import sleep #allows us to delay things a little
+from random import choice 
 
-# For each quote you should grab the text of the quote, 
-# the name of the person who said the quote, 
-# and the href of the link to the person's bio. Store all of this information in a list.
+BASE_URL = 'http://quotes.toscrape.com'
 
-# Next, display the quote to the user and ask who said it. 
-# The player will have four guesses remaining.
-# After each incorrect guess, the number of guesses remaining will decrement. 
-# If the player gets to zero guesses without identifying the author, 
-# the player loses and the game ends. If the player correctly identifies the author, 
-# the player wins!
+def scrape_quotes():
+    all_quotes = []
+    url = "/page/1"
+    while url: 
+        response = requests.get(BASE_URL+url)
+        print(f"Scraping: {BASE_URL}{url}")
+        soup = BeautifulSoup(response.text, "html.parser") #getting the soup version that is nav 
 
-# After every incorrect guess, the player receives a hint about the author. 
-# For the first hint, make another request to the author's bio page 
-# (this is why we originally scrape this data), and tell the player 
-# the author's birth date and location.
+        quotes = soup.find_all(class_="quote")
+        for quote in quotes:
+            all_quotes.append({
+                "text": quote.find(class_="text").text,
+                "author": quote.find(class_="author").text,
+                "link": quote.find("a")["href"]
+            })
 
-# The next two hints are up to you! 
-# Some ideas: the first letter of the author's first name, 
-# the first letter of the author's last name, the number of letters in one of the names, etc.
+        next_btn = soup.find(class_= "next") 
+        url = next_btn.find("a")["href"] if next_btn else None
+        # sleep(1)
+        return all_quotes
 
-# When the game is over, ask the player if they want to play again. 
-# If yes, restart the game with a new quote. If no, the program is complete.
+def start_game(quotes):
+    quote = choice(quotes)
+    rem_guesses = 4
+    guess = ""
+
+    print("Here is a quote: ")
+    print(quote["text"])
+
+    while guess.lower() != quote["author"].lower() and rem_guesses:
+        guess = input(f"Who said this? *guesses remaining {rem_guesses}\n")
+        if guess.lower() == quote["author"].lower():
+            print("Right!")
+            break
+
+        rem_guesses -= 1
+        if rem_guesses == 3:
+            r = requests.get(f'{BASE_URL}{quote["link"]}')
+            soup = BeautifulSoup(r.text, 'html.parser')
+            dob = soup.find(class_="author-born-date").text
+            print(f"They were born on: {dob}")
+        elif rem_guesses == 2:
+            r = requests.get(f'{BASE_URL}{quote["link"]}')
+            soup = BeautifulSoup(r.text, 'html.parser')
+            pob = soup.find(class_="author-born-location").text
+            print(f"They were born in: {pob}")
+        elif rem_guesses == 1:
+            last_initial = quote["author"].split(" ")[1][0]
+            print(f"Their name starts with a '{quote['author'][0]}' and a '{last_initial}'")
+        else:
+            print(f"Sorry, you ran out of guesses. It was: {quote['author']}")
+
+    play = ''
+    while play.lower() not in ('y', 'yes', 'n', 'no'):
+        play = input("Play Again? ")
+    if play.lower() in ('y', 'yes'):
+        return start_game(quotes)
+    else:
+        print("Okay, Bye then.")
+
+quotes = scrape_quotes()
+start_game(quotes)
